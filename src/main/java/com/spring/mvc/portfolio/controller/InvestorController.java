28452bb5eee5df717fbb8881fafaad300a6ae43e
qw851210@gmail.com <>
2021-01-16 12:31:07 +0800

    SpringData1114_Portfolio

--- src/main/java/com/spring/mvc/portfolio/controller/InvestorController.java
+++ src/main/java/com/spring/mvc/portfolio/controller/InvestorController.java
@@ -7,6 +7,7 @@ import com.spring.mvc.portfolio.service.EmailService;
 import com.spring.mvc.portfolio.service.PortfolioService;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,45 +19,55 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController  // @Controller + @ResponseBody
-@RequestMapping("/portfolio/investor/")
+@RestController // @Controller + @ResponseBody
+@RequestMapping("/portfolio/investor")
 public class InvestorController {
-    
-    @Autowired
-    private EmailService emailservice;
-    
-    
     @Autowired
     private PortfolioService service;
+    
+    @Autowired
+    private EmailService emailService;
+    
     // 新增
-    @PostMapping(value = {"/","/add"})
-    public Investor add(@RequestBody Map<String , String > jsonMap){
-        //jsonMap 就是前端傳來的 json 字串 所轉換後的集合資料
-        //建立 Investor
-        Investor investor =new Investor();
+    @PostMapping(value = {"/", "/add"})
+    public Investor add(@RequestBody Map<String, String> jsonMap) {
+        // jsonMap 就是前端傳來的 json 字串所轉換後的集合資料
+        // {"username":"admin","password":"1234","email":"vincentjava@yahoo.com.tw","balance":"200"}
+        // 建立 Investor
+        Investor investor = new Investor();
         investor.setUsername(jsonMap.get("username"));
         investor.setPassword(jsonMap.get("password"));
         investor.setEmail(jsonMap.get("email"));
         investor.setBalance(Integer.parseInt(jsonMap.get("balance")));
         investor.setPass(Boolean.FALSE);
-        //設定 email 認證碼
+        // 設定 email 認證碼
         investor.setCode(Integer.toHexString(investor.hashCode()));
-        //建立 watch
+        
+        // 建立 Watch
         Watch watch = new Watch();
         watch.setInvestor(investor);
-        watch.setName(investor.getUsername() +"的投資組合");
-        //存檔  Investor
+        watch.setName(investor.getUsername() + "的投資組合");
+        // 存檔 Investor
         service.getInvestorRepository().save(investor);
-        //存檔  watch
+        // 存檔 Watch
         service.getWatchRepository().save(watch);
         
-        //發送認證信件
-        emailservice.send(investor);
-        return investor;
+        // 發送認證信件
+        emailService.send(investor);
         
+        return investor;
         
     }
     
+    // 查詢全部
+    @GetMapping(value = {"/", "/query"})
+    public List<Investor> queryAll() {
+        return service.getInvestorRepository().findAll();
+    }
     
-    
+    // 查詢單筆(根據 id)
+    @GetMapping(value = {"/{id}"})
+    public Investor get(@PathVariable("id") Optional <Integer> id){
+        return service.getInvestorRepository().findOne(id.get());
+    }
 }
